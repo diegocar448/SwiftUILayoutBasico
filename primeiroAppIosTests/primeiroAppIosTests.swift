e3fb29dc52f5d8f8931dba30c7c16f7f72926ce9
Diego Cardoso <diegocar448@hotmail.com>
2025-03-12 15:11:12 -0300

    Initial Commit

---
+++ primeiroAppIosTests/primeiroAppIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  primeiroAppIosTests.swift
+//  primeiroAppIosTests
+//
+//  Created by Diego Cardoso on 12/03/25.
+//
+
+import Testing
+@testable import primeiroAppIos
+
+struct primeiroAppIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
