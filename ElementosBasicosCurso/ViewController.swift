07320cdfd635674ba3d0361f5ad01c1787628743
Diego Cardoso <diegocar448@hotmail.com>
2025-03-17 14:34:06 -0300

    board and code

--- ElementosBasicosCurso/ViewController.swift
+++ ElementosBasicosCurso/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Ligação IBOutlet para referenciar as caracteristica de um elemento
+    @IBOutlet weak var bemVindoLabel: UILabel!    
 
+    
+    //ciclo de vida - metodo inicial
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bemVindoLabel.text = "Teste UILabel"
     }
 
 
