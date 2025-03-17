93fd589c8b017dbd1b6518278e656985dc152532
Diego Cardoso <diegocar448@hotmail.com>
2025-03-17 16:12:24 -0300

    other project new files

---
+++ PrimeiraTelaCadastroCursoTests/PrimeiraTelaCadastroCursoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiraTelaCadastroCursoTests.swift
+//  PrimeiraTelaCadastroCursoTests
+//
+//  Created by Diego Cardoso on 17/03/25.
+//
+
+import Testing
+@testable import PrimeiraTelaCadastroCurso
+
+struct PrimeiraTelaCadastroCursoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
