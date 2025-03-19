6e2f70febb0375941908edaf18baf8f3929ad191
Diego Cardoso <diegocar448@hotmail.com>
2025-03-19 16:20:18 -0300

    UISwitch start

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Diego Cardoso on 19/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UISwitchTests/UISwitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  UISwitchTests.swift
+//  UISwitchTests
+//
+//  Created by Diego Cardoso on 19/03/25.
+//
+
+import Testing
+@testable import UISwitch
+
+struct UISwitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
