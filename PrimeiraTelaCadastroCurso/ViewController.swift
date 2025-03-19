996d697c4fb02e54af329c4b5a6382b38f2d8759
Diego Cardoso <diegocar448@hotmail.com>
2025-03-19 13:52:32 -0300

    set em tempo de execucao / funcoes nativas

--- PrimeiraTelaCadastroCurso/ViewController.swift
+++ PrimeiraTelaCadastroCurso/ViewController.swift
@@ -17,16 +17,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //setar campos
         nameTextField.keyboardType = .default
         emailTextField.keyboardType = .emailAddress
         passwordTextField.keyboardType = .default
         passwordTextField.isSecureTextEntry = true
+        
+        
+        //setar props durante a execucao
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
-    
     
     @IBAction func tappedRegisterButton(_ sender: UIButton) {
     }
-    
+}
 
+extension ViewController: UITextFieldDelegate{
+    
+    // primeiro metodo disparado
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        print(#function)
+        textField.layer.borderColor = UIColor.blue.cgColor
+        //focusin
+        textField.layer.borderWidth = 1
+    }
+   
+    //teclado ao fechar
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        //focusOut
+        textField.layer.borderWidth = 0
+
+    }
+    
+    //teclado ao abrir
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        textField.resignFirstResponder()
+
+        return true
+    }
 }
 
