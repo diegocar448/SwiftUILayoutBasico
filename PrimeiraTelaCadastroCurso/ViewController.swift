bf4f8ca79d01a028a284e7a94ed6bedbc1cac126
Diego Cardoso <diegocar448@hotmail.com>
2025-03-19 10:55:40 -0300

    custom cadastro

--- PrimeiraTelaCadastroCurso/ViewController.swift
+++ PrimeiraTelaCadastroCurso/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.keyboardType = .default
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.keyboardType = .default
+        passwordTextField.isSecureTextEntry = true
     }
-
+    
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+    }
+    
 
 }
 
