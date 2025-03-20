fb7cfdfa1936d8dc00cfa4d697ad4a9e8eeddb70
Diego Cardoso <diegocar448@hotmail.com>
2025-03-20 13:38:39 -0300

    UiSlider end

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slider.minimumValue = 0
+        slider.value = 0
+        slider.maximumValue = 20
+        slider.minimumTrackTintColor = .red
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = .green
     }
-
+    
+    
+    @IBAction func tappedSlider(_ sender: UISlider) {
+        print(Int(sender.value))
+    }
+    
 
 }
 
