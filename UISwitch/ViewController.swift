64f8cbcbe417c33a6630ac71425e143febb3dd42
Diego Cardoso <diegocar448@hotmail.com>
2025-03-20 09:42:37 -0300

    UISwitch

--- UISwitch/ViewController.swift
+++ UISwitch/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stateSwitch.isOn = true
+        stateSwitch.onTintColor = .blue
     }
 
 
+    @IBAction func tappedSwitchAction(_ sender: UISwitch) {
+        print(sender.isOn)
+        
+        if sender.isOn {
+            view.backgroundColor = .green
+        }else{
+            view.backgroundColor = .red
+        }
+    }
 }
 
+	
