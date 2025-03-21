a477ee00bf2d49f8ac6551f157830397edc13fc2
Diego Cardoso <diegocar448@hotmail.com>
2025-03-21 15:11:55 -0300

    Navigation Controller start

---
+++ NavigationControllerTests/NavigationControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerTests.swift
+//  NavigationControllerTests
+//
+//  Created by Diego Cardoso on 21/03/25.
+//
+
+import Testing
+@testable import NavigationController
+
+struct NavigationControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
